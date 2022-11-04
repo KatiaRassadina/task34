d6066451de96105451ab239c7c5ccfb6279e7703
KatiaRassadina <rassadinaekaterina877@gmail.com>
2022-11-04 20:06:17 +0300

    task done

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив, заполненный случайными положительными 3-значными числами. Напишите программу, которая покажет кол-во четных чисел в массиве
+int[]array = new int[10];
+FillArray(array);
+PrintArray(array);
+SumEvenNumbers(array);
+void FillArray(int[] array)
+{
+    for (int i=0; i<array.Length; i++)
+    {
+        array[i] = new Random.Next(100,1000);
+    }
+}
+void PrintArray(int[] array);
+Console.WriteLine($"{array[]}");
+void SunEvenNumbers(int[] array)
+{
+for (int i=0; i<array.Length; i++)
+sum++;
+}
+Console.WriteLine("The numver of even digits is" +sum);
